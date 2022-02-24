378b608655685807b79d8621f4242d15f898a582
Manikandan <dineshmani1999@gmail.com>
2022-02-24 09:38:02 +0530

    updated version

--- welcome.py
+++ welcome.py
@@ -3,3 +3,8 @@ if a == "yes":
     print("ok friend")
 else:
     print("ok brother")
+b= input("did you had your dinner")
+if b == " yes":
+    print("lets go for a party")
+else:
+    print("lets go for a dinner")
